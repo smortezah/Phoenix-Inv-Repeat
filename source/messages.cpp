6f86b491bb6d83269e401ef8ee506b3468100283
Morteza Hosseini <s_morteza_h@yahoo.com>
2016-11-08 18:07:22 +0000

    messages main

--- source/messages.cpp
+++ source/messages.cpp
@@ -4,32 +4,34 @@
 #include <iostream>
 
 
-// shows usage guide
+////  shows usage guide ////////////////////////////////////////////
 void PrintUsageGuide (void)
 {
     std::cout
-            << "Synopsis:"                                        << "\n"
-            << "    phoenix [OPTION]... [FILE1] [FILE2]"          << "\n"
-                                                                  << "\n"
-            << "Options:"                                         << "\n"
-            << "    -h,  --help"                                  << "\n"
-            << "         usage guide"                             << "\n"
-                                                                  << "\n"
-            << "    -V,  --version"                               << "\n"
-            << "         version number"                          << "\n"
-                                                                  << "\n"
-            << "    -v,  --verbose"                               << "\n"
-            << "         verbose mode (more information)"         << "\n"
-                                                                  << "\n"
-            << "    [FILE1]"                                      << "\n"
-            << "         reference file"                          << "\n"
-                                                                  << "\n"
-            << "    [FILE2]"                                      << "\n"
-            << "         target file"                             << "\n";
+            << "Synopsis:"                                  << "\n"
+            << "    phoenix [OPTION]... [FILE1] [FILE2]"    << "\n"
+                                                            << "\n"
+            << "Options:"                                   << "\n"
+            << "    -h,  --help"                            << "\n"
+            << "         usage guide"                       << "\n"
+                                                            << "\n"
+            << "    -V,  --version"                         << "\n"
+            << "         version number"                    << "\n"
+                                                            << "\n"
+            << "    -v,  --verbose"                         << "\n"
+            << "         verbose mode (more information)"   << "\n"
+                                                            << "\n"
+            << "    -n,  --number"                          << "\n"
+            << "         number"                            << "\n"
+                                                            << "\n"
+            << "    [FILE1]"                                << "\n"
+            << "         reference file"                    << "\n"
+                                                            << "\n"
+            << "    [FILE2]"                                << "\n"
+            << "         target file"                       << "\n";
 }
 
-
-// shows version number
+////  shows version number ////////////////////////////////////////////
 void PrintVersion(void)
 {
     std::cout
--- source/phoenix.cpp
+++ source/phoenix.cpp
@@ -1,58 +1,127 @@
 #include <iostream>
 #include <unistd.h>
+#include <vector>
+#include <array>
+#include <string>
 
 #include "def.h"
 #include "messages.h"
 
 
-int32_t main (int argc, char *argv[])
+
+
+/* Flag set by ‘--verbose’. */
+static int verbose_flag;
+
+
+
+
+////  command line parser ////////////////////////////////////////////
+int32_t CommandLineParser (int argc, char **argv)
 {
-    PrintUsageGuide();
-    
-    PrintVersion();
-    
-    
-    
-    
-    
-    
-    int aflag = 0;
-    int bflag = 0;
-    char *cvalue = NULL;
     int index;
     int c;
     
+    
+    std::string numberStr;
+    
     opterr = 0;
-    while ((c = getopt (argc, argv, "abc:")) != -1)
+    while ((c = getopt(argc, argv, "hVvn:")) != -1)
         switch (c)
         {
-            case 'a':
-                aflag = 1;
+            case 'h':
+                PrintUsageGuide();
                 break;
-            case 'b':
-                bflag = 1;
+            
+            case 'V':
+                PrintVersion();
                 break;
-            case 'c':
-                cvalue = optarg;
+            
+            case 'v':
                 break;
+            
+            case 'n':
+                numberStr = optarg;
+                break;
+            
+            
+            
+            
             case '?':
                 if (optopt == 'c')
-                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-                else if (isprint (optopt))
-                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint(optopt))
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
                 else
-                    fprintf (stderr,
-                             "Unknown option character `\\x%x'.\n",
-                             optopt);
+                    fprintf(stderr,
+                            "Unknown option character `\\x%x'.\n",
+                            optopt);
                 return 1;
+            
             default:
-                abort ();
+                abort();
         }
-    printf ("aflag = %d, bflag = %d, cvalue = %s\n",
-            aflag, bflag, cvalue);
+    
+    std::cout << numberStr;
+    
+    
+    
     
     for (index = optind; index < argc; index++)
-        printf ("Non-option argument %s\n", argv[index]);
+        printf("Non-option argument %s\n", argv[ index ]);
+}
+
+
+
+///////////////////////////////////////////////////////////////
+///////////                 M A I N                 ///////////
+///////////////////////////////////////////////////////////////
+
+int32_t main (int argc, char *argv[])
+{
+    CommandLineParser(argc, argv);
+    
+    
+    
+
+//
+//    int aflag = 0;
+//    int bflag = 0;
+//    char *cvalue = NULL;
+//    int index;
+//    int c;
+//
+//    opterr = 0;
+//    while ((c = getopt (argc, argv, "abc:")) != -1)
+//        switch (c)
+//        {
+//            case 'a':
+//                aflag = 1;
+//                break;
+//            case 'b':
+//                bflag = 1;
+//                break;
+//            case 'c':
+//                cvalue = optarg;
+//                break;
+//            case '?':
+//                if (optopt == 'c')
+//                    fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+//                else if (isprint (optopt))
+//                    fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+//                else
+//                    fprintf (stderr,
+//                             "Unknown option character `\\x%x'.\n",
+//                             optopt);
+//                return 1;
+//            default:
+//                abort ();
+//        }
+//    printf ("aflag = %d, bflag = %d, cvalue = %s\n",
+//            aflag, bflag, cvalue);
+//
+//    for (index = optind; index < argc; index++)
+//        printf ("Non-option argument %s\n", argv[index]);
     
     
     
