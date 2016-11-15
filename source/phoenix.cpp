f1d400bf800ce4156be8e168933ab4b7902109fd
Morteza Hosseini <s_morteza_h@yahoo.com>
2016-11-15 14:50:52 +0000

    .

--- source/phoenix.cpp
+++ source/phoenix.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <getopt.h>
+#include <fstream>
 #include <cstring>
 #include <stdint.h>
 #define __STDC_FORMAT_MACROS
@@ -22,6 +23,32 @@
 int32_t main (int argc, char *argv[])
 {
 //    Functions::commandLineParser(argc, argv);
+    
+
+    
+/***********************************************************
+    for test
+************************************************************/
+    
+    // file opened
+    std::ifstream myFile("a.fa", std::ios::in);
+    
+    if (!myFile)
+    {
+        std::cerr << "File could not be opened.\n";
+        return 1;
+    }
+    
+    std::string strEachLine;
+    std::string strDataset;
+    
+    while (std::getline(myFile, strEachLine))
+        strDataset += strEachLine;
+
+    std::cout << strDataset << std::endl;
+    
+    myFile.close();
+    // file closed
     
     
     
