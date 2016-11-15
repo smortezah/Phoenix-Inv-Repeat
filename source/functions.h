c9ec60e3fb4355646022c4d92e977d11e2be9565
Morteza Hosseini <s_morteza_h@yahoo.com>
2016-11-15 11:56:48 +0000

    .

---
+++ source/functions.h
@@ -0,0 +1,15 @@
+#ifndef PHOENIX_FUNCTIONS_H
+#define PHOENIX_FUNCTIONS_H
+
+#include <iostream>
+
+
+class Functions
+{
+public:
+    Functions ();                                   // constructor
+    static int32_t commandLineParser (int, char**); // command line parser
+};
+
+
+#endif //PHOENIX_FUNCTIONS_H
