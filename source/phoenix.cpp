dab86a68cad2dbdb965060e379342b9cd1d8da14
Morteza Hosseini <s_morteza_h@yahoo.com>
2016-11-16 15:59:01 +0000

    .

--- source/phoenix.cpp
+++ source/phoenix.cpp
@@ -17,10 +17,28 @@
 
 
 
+
 /***********************************************************
     for test
 ************************************************************/
 #include <array>
+
+int baseCharToNum(std::string s)
+{
+    std::string intStr(s);
+    
+    for (size_t i = 0; i < s.size(); ++i)
+        switch (s[ i ])
+        {
+            case 'A':   intStr[ i ] = '0';    break;
+            case 'C':   intStr[ i ] = '1';    break;
+            case 'T':   intStr[ i ] = '2';    break;
+            case 'G':   intStr[ i ] = '3';    break;
+            default:    break;
+        }
+    
+    return std::stoi(intStr);
+}
 
 
 
@@ -39,7 +57,7 @@ int32_t main (int argc, char *argv[])
 ************************************************************/
     
     // file opened
-    std::ifstream myFile("a.fa", std::ios::in);
+    std::ifstream myFile("c.fa", std::ios::in);
     
     if (!myFile)
     {
@@ -53,20 +71,54 @@ int32_t main (int argc, char *argv[])
     while (std::getline(myFile, strEachLine))
         strDataset += strEachLine;
 
-//    std::cout << strDataset << std::endl;
+    std::cout << strDataset << std::endl;
     
-    myFile.close();
-    // file closed
+    myFile.close(); // file closed
     
-    uint8_t CONTEXT_DEPTH = 2;
-    uint8_t ROW = 16;
-    uint8_t COL = 9;
     
+    const int CONTEXT_DEPTH = 2;
+    const uint8_t ROW = 16;
+    const uint8_t COL = 4;
+
     int table[ROW][COL];
+    memset(table, 0, sizeof(table[ 0 ][ 0 ]) * ROW * COL);
+
+
+    char DNAbase[4] = {'A', 'C', 'T', 'G'};
+    
+    std::string context (CONTEXT_DEPTH, '0');
+    int index = 0;
+    
+    strDataset = context + strDataset;
+
+    for (size_t i = CONTEXT_DEPTH; i < strDataset.size()-1; ++i)
+    {
+        switch (strDataset[ i ])
+        {
+            case 'A':   table[ index ][ 0 ] += 1;   break;
+            case 'C':   table[ index ][ 1 ] += 1;   break;
+            case 'T':   table[ index ][ 2 ] += 1;   break;
+            case 'G':   table[ index ][ 3 ] += 1;   break;
+            default:    break;
+        }
+    
+        context = strDataset.substr(i - CONTEXT_DEPTH + 1, CONTEXT_DEPTH);
+
+        index = baseCharToNum(context);
+        
+        std::cout << context << " " << index << "\n";
+    }
+    
+
     
     for (size_t i = 0; i < ROW; ++i)
+    {
+        std::cout << "[" << i << "]:\t";
         for (size_t j = 0; j < COL; ++j)
-            table[ i ][ j ] = 0;
+            std::cout << table[ i ][ j ] << "\t";
+
+        std::cout << "\n";
+    }
     
     
     return 0;
