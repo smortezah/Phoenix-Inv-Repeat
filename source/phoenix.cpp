91800c7968de8de4013e9fa0aa77b010ffab4f46
Morteza Hosseini <s_morteza_h@yahoo.com>
2016-11-18 17:49:33 +0000

    .

--- source/phoenix.cpp
+++ source/phoenix.cpp
@@ -34,6 +34,8 @@
 
 const std::string alphabet{"ACTG"};
 const uint8_t CONTEXT_DEPTH = 2;
+const uint8_t ALPHA_NUMERATOR = 1;
+const uint8_t ALPHA_DENUMERATOR = 1;
 const uint8_t COL = 4;  // number of columns of the table
 
 static std::vector< std::string > permuteVector;
@@ -66,7 +68,7 @@ int32_t main (int argc, char *argv[])
     for test
 ************************************************************/
     // file opened
-    std::ifstream myFile("b.fa", std::ios::in);
+    std::ifstream myFile("c.fa", std::ios::in);
     
     if (!myFile)
     {
@@ -86,47 +88,81 @@ int32_t main (int argc, char *argv[])
     myFile.close();
     // file closed
     
+    
     // all permutations of the alphabet with the CONTEXT_SIZE size
     int alphabetSize = (int) alphabet.size();
     permutation(alphabet, "", alphabetSize, CONTEXT_DEPTH);
 //    for (std::string s : permuteVector)
 //        std::cout << s << "\n";
-    
+
     // a table to save the number of occurrences
     int ROW = pow(alphabetSize, CONTEXT_DEPTH);
     int table[ROW][COL];
     memset(table, 0, sizeof(table[ 0 ][ 0 ]) * ROW * COL);
-    
+
     // context, that slides in the dataset
     std::string context(CONTEXT_DEPTH, 'A');
     int index = 0;
-    
+
     // add "AA..." (with the size of CONTEXT_DEPTH) to the beginning of the dataset
     strDataset = context + strDataset;
-    
+
     // an iterator for iterating in the permuteVector
     std::vector< std::string >::iterator indexIterator;
+
+    int counters[4];    memset(counters, 0, sizeof(counters[ 0 ]) * 4);
+    int totalCount = 0;
+    int symbol = 0;
+    FILE *writer = fopen("mori", "w");
     
-    // add 1 to the number of occurrences if a specific symbol is seen
     for (size_t i = CONTEXT_DEPTH; i < strDataset.size(); ++i)
     {
+        // add 1 to the number of occurrences if a specific symbol is seen
         switch (strDataset[ i ])
         {
-            case 'A':   table[ index ][ 0 ] += 1;   break;
-            case 'C':   table[ index ][ 1 ] += 1;   break;
-            case 'T':   table[ index ][ 2 ] += 1;   break;
-            case 'G':   table[ index ][ 3 ] += 1;   break;
-            default:    break;
+            case 'A':
+                table[ index ][ 0 ] += 1;
+                counters[ 0 ] += ALPHA_DENUMERATOR * table[ index ][ 0 ] + ALPHA_NUMERATOR;
+                break;
+                
+            case 'C':
+                table[ index ][ 1 ] += 1;
+                counters[ 1 ] += ALPHA_DENUMERATOR * table[ index ][ 1 ] + ALPHA_NUMERATOR;
+                break;
+                
+            case 'T':
+                table[ index ][ 2 ] += 1;
+                counters[ 2 ] += ALPHA_DENUMERATOR * table[ index ][ 2 ] + ALPHA_NUMERATOR;
+                break;
+                
+            case 'G':
+                table[ index ][ 3 ] += 1;
+                counters[ 3 ] += ALPHA_DENUMERATOR * table[ index ][ 3 ] + ALPHA_NUMERATOR;
+                totalCount = (ALPHA_DENUMERATOR * (table[ index ][ 0 ] + table[ index ][ 1 ] +
+                                                   table[ index ][ 2 ] + table[ index ][ 3 ])) +
+                             (4 * ALPHA_NUMERATOR);
+//                AESym(3, (int*) counters, totalCount, writer);
+                break;
+                
+            default:
+                break;
         }
-    
+        
+//        totalCount = (ALPHA_DENUMERATOR * (table[ index ][ 0 ] + table[ index ][ 1 ] +
+//                                           table[ index ][ 2 ] + table[ index ][ 3 ])) +
+//                     (4 * ALPHA_NUMERATOR);
+//
+//        memset(counters, 0, sizeof(counters[ 0 ]) * 4);
+            
+        
         // slides in the dataset
         context = strDataset.substr(i - CONTEXT_DEPTH + 1, CONTEXT_DEPTH);
-        
+
         // index of the context
         indexIterator = std::find(permuteVector.begin(), permuteVector.end(), context);
         index = indexIterator - permuteVector.begin();
     }
-    
+
     // show the table
     for (size_t i = 0; i < ROW; ++i)
     {
@@ -136,7 +172,7 @@ int32_t main (int argc, char *argv[])
         std::cout << "\n";
     }
     std::cout << "*************************************************\n";
-    
+
     
     return 0;
 }
