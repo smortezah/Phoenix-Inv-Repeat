549530d62fda6566c95e5935075061de78780430
smortezah <s_morteza_h@yahoo.com>
2016-11-29 16:05:47 +0000

    .

--- source/FCM.cpp
+++ source/FCM.cpp
@@ -10,9 +10,15 @@ FCM::FCM () {}
 /***********************************************************
     getters and setters
 ************************************************************/
+// getter of context depth
 uint8_t FCM::getContextDepth () const { return contextDepth; }
+// setter of context depth
 void FCM::setContextDepth (uint8_t contextDepth) { FCM::contextDepth = contextDepth; }
+// getter of alpha denominator
 uint32_t FCM::getAlphaDenom () const { return alphaDenom; }
+// setter of alpha denominator
 void FCM::setAlphaDenom (uint32_t alphaDenom) { FCM::alphaDenom = alphaDenom; }
-const hashTable_t &FCM::getHTable () const { return hTable; }
-void FCM::setHTable (const hashTable_t &hTable) { FCM::hTable = hTable; }
+// getter of hash table
+const hashTable_t& FCM::getHTable () const { return hTable; }
+// setter of hash table
+void FCM::setHTable (const hashTable_t& hTable) { FCM::hTable = hTable; }
--- source/FCM.h
+++ source/FCM.h
@@ -10,19 +10,19 @@
 class FCM
 {
 public:
-    FCM ();
+    FCM ();                                         // constructor
     
-    uint8_t getContextDepth () const;
-    void setContextDepth (uint8_t contextDepth);
-    uint32_t getAlphaDenom () const;
-    void setAlphaDenom (uint32_t alphaDenom);
-    const hashTable_t& getHTable () const;
-    void setHTable (const hashTable_t &hTable);
+    uint8_t getContextDepth () const;               // getter of context depth
+    void setContextDepth (uint8_t contextDepth);    // setter of context depth
+    uint32_t getAlphaDenom () const;                // getter of alpha denominator
+    void setAlphaDenom (uint32_t alphaDenom);       // setter of alpha denominator
+    const hashTable_t& getHTable () const;          // getter of hash table
+    void setHTable (const hashTable_t& hTable);     // setter of hash table
 
 private:
-    uint8_t     contextDepth;   // context depth
-    uint32_t    alphaDenom;     // alpha denominator
-    hashTable_t hTable;         // hash table
+    uint8_t contextDepth;                           // context depth
+    uint32_t alphaDenom;                            // alpha denominator
+    hashTable_t hTable;                             // hash table
 };
 
 
