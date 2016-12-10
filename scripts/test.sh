3373b190646d70262a6afd846cc4092381a3b88b
Morteza Hosseini <s_morteza_h@yahoo.com>
2016-12-10 19:30:21 +0000

    Add files via upload

--- scripts/test.sh
+++ scripts/test.sh
@@ -17,3 +17,15 @@ do
 		done
 	done
 done
+
+for FILE in ir.dat
+do
+    gnuplot <<- EOF
+        set xlabel "Label"
+        set ylabel "Label2"
+        set title "Graph title"   
+        set term png
+        set output "${FILE}.png"
+        plot "${FILE}" using 2:4
+EOF
+done
