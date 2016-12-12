3ac46b4d9ea474dbf345fc9aaf28b181f29bde21
smortezah <s_morteza_h@yahoo.com>
2016-12-12 15:00:01 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -13,25 +13,28 @@ maxCtx=21   # real: -=1
 
 for ir in 0 #1
 do
-    for alphaDen in 1 #10 100
+    for alphaDen in 1 10 100
     do
     rm -f $irName$ir$aName$alphaDen.dat
     touch $irName$ir$aName$alphaDen.dat
     echo -e "# ir\talpha\tctx\tbpb" >> $irName$ir$aName$alphaDen.dat
-        for((ctx=2;ctx<$maxCtx;++ctx))
+        for((ctx=2; ctx<$maxCtx; ++ctx))
         do
         ./phoenix -m t,$ctx,$alphaDen,$ir -t b.fa >> $irName$ir$aName$alphaDen.dat
         done
     done
 done
 
-for FILE in $irName*.dat
+for FILE in ir0ad1.dat ir0ad10.dat ir0ad100.dat
 do
+#filenames="ir0ad1.dat ir0ad10.dat ir0ad100.dat"
 gnuplot <<- EOF
 set xlabel "context"
 set ylabel "bpb"
+set key right bottom
 set term $PIXFORMAT
 set output "${FILE}.$PIXFORMAT"
-plot "${FILE}" using 3:4 with linespoints ls 7
+#plot for [file in filenames] file."dat" using 3:4 with lines
+plot "${FILE}" using 3:4 with linespoints ls 7 title "${FILE}"
 EOF
 done
