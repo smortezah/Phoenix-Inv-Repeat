f821dfcf34807dad84894060e6c29514f1c88779
smortezah <s_morteza_h@yahoo.com>
2016-12-12 16:04:57 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,16 +11,19 @@ irName=ir
 aName=ad    # alpha name
 maxCtx=21   # real: -=1
 
-for ir in 0 1
+for dataset in c #b a
 do
-    for alphaDen in 1 10 100
+    for ir in 0 1
     do
-    rm -f $irName$ir$aName$alphaDen.dat
-    touch $irName$ir$aName$alphaDen.dat
-    echo -e "# ir\talpha\tctx\tbpb" >> $irName$ir$aName$alphaDen.dat
-        for((ctx=2; ctx<$maxCtx; ++ctx))
+        for alphaDen in 1 10 100
         do
-        ./phoenix -m t,$ctx,$alphaDen,$ir -t b.fa >> $irName$ir$aName$alphaDen.dat
+        rm -f $irName$ir$aName$alphaDen.dat
+        touch $irName$ir$aName$alphaDen.dat
+        echo -e "# ir\talpha\tctx\tbpb" >> $irName$ir$aName$alphaDen.dat
+            for((ctx=2; ctx<$maxCtx; ++ctx))
+            do
+            ./phoenix -m t,$ctx,$alphaDen,$ir -t $dataset.fa >> $irName$ir$aName$alphaDen.dat
+            done
         done
     done
 done
@@ -31,7 +34,6 @@ set ylabel "bpb"
 set key right bottom
 set term $PIXFORMAT
 set output "ir0.dat.$PIXFORMAT"
-#plot "${FILE}" using 3:4 with linespoints ls 7 title "${FILE}"
 plot    "ir0ad1.dat" using 3:4  with linespoints ls 6 title "ir=0, alpha=1/1    ",\
         "ir0ad10.dat" using 3:4 with linespoints ls 7 title "ir=0, alpha=1/10  ",\
         "ir0ad100.dat" using 3:4 with linespoints ls 8 title "ir=0, alpha=1/100"
