24e8b7d5e57b7928a20f8b99e72e3e30feab6f77
smortezah <s_morteza_h@yahoo.com>
2016-12-12 10:54:48 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,7 +4,9 @@ cd ..
 cmake ./source
 make
 
-<<<<<<< HEAD
+PIXFORMAT=png
+#PIXFORMAT=svg
+
 # ir = 0
 ir=0
 alphaDen=1
@@ -16,46 +18,20 @@ do
 echo -e "# ir\talpha\tctx\tbpb" >> $file.dat
 done
 
-for((ctx=2;ctx<20;++ctx))
+for((ctx=2;ctx<21;++ctx))
 do
 ./phoenix -m t,$ctx,$alphaDen,$ir -t b.fa >> ir$ir.dat
 done
 
 for FILE in ir$ir.dat
 do
-    gnuplot <<- EOF
-        set xlabel "context"
-        set ylabel "bpb"
-        #set title "Graph title"
-        set term png
-        set output "${FILE}.png"
-        plot "${FILE}" using 3:4
+gnuplot <<- EOF
+    set xlabel "context"
+    set ylabel "bpb"
+    set style func linespoints
+#    show style line
+    set term $PIXFORMAT
+    set output "${FILE}.$PIXFORMAT"
+    plot "${FILE}" using 3:4
 EOF
 done
-=======
-touch ir.dat
-echo -e "# ctx\talpha\tir\tbpb" >> ir.dat
-
-for ir in 0 1
-do
-	for alphaDen in 1
-	do
-		for ctx in 2 3 4
-		do
-		./phoenix -m t,$ctx,$alphaDen,$ir -t b.fa >> ir.dat
-		done
-	done
-done
-
-for FILE in ir.dat
-do
-    gnuplot <<- EOF
-        set xlabel "Label"
-        set ylabel "Label2"
-        set title "Graph title"   
-        set term png
-        set output "${FILE}.png"
-        plot "${FILE}" using 2:4
-EOF
-done
->>>>>>> origin/master
