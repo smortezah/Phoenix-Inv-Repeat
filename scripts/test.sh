e6ce7d8202df4c277531f8a21936120c3219f047
smortezah <s_morteza_h@yahoo.com>
2016-12-14 20:18:10 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,20 +9,20 @@ make
 
 
 #***********************************************************
-#   parameters to install and run needed programs, and generate datasets
+#   parameters to install and run needed programs
 #***********************************************************
 GEN_DATASET=0   # generate dataset using "XS"
 INSTALL_XS=0    # to install "XS" from Github
 INSTALL_goose=0 # to install "goose" from Github
 RUN=1           # run the program
 
-numDatasets=3   # number of datasets to be generated
-
 
 #***********************************************************
 #   generate dataset using "XS" and "goose"
 #***********************************************************
 if [[ $GEN_DATASET == 1 ]]; then
+
+numDatasets=3   # number of datasets to be generated
 
 # dataset names: nonRep=non repetitve,  midRep=mid repetitve,   tooRep=too repetitve
 
@@ -57,7 +57,9 @@ if [[ $GEN_DATASET == 1 ]]; then
     cd ../../
     fi  # end of installing "goose"
 
-# generate the original sequence ("nonRep0")
+#-----------------------------------
+#   generate the original sequence ("nonRep0")
+#-----------------------------------
 XS/XS -ls 100 -n 100000 -rn 0 -f 0.20,0.20,0.20,0.20,0.20 -eh -eo -es nonRep0   # non-repetitive
 
 # add ">X" as the header of the sequence (build "nonRepX")
@@ -65,47 +67,71 @@ echo ">X" > HEADER;
 cat HEADER nonRep0 > nonRepX;
 rm -f HEADER
 
-# generate the mutated sequences
-for((x=1 ; x!=$((numDatasets+1)) ; ++x));
+#-----------------------------------
+#   generate the mutated sequences
+#-----------------------------------
+for((x=1; x!=$((numDatasets+1)); ++x));
 do
-MRATE=`echo "scale=3;$x/100" | bc -l`;
+MRATE=`echo "scale=3;$x/100" | bc -l`;      # handle transition 0.09 -> 0.10
 #echo "Substitutions rate: $MRATE";
 goose/src/goose-mutatefasta -s $x -a5 -mr $MRATE " " < nonRepX > nonRepTemp$x;
 cat nonRepTemp$x | grep -v ">" > nonRep$x   # remove the header line
 done
-rm -f nonRepX nonRepTemp*   # remove temporary files
+rm -f nonRepX nonRepTemp*                   # remove temporary files
 
-# move all generated files to "datasets" folder
+#-----------------------------------
+#   move all generated dataset files to "datasets" folder
+#-----------------------------------
 rm -rf datasets
 mkdir -p datasets
 mv ./nonRep* datasets
+
 fi  # end of generating dataset
 
-# running the program
+
+#***********************************************************
+#   running the program
+#***********************************************************
 if [[ $RUN == 1 ]]; then
-# list of datasets
-tooRep="tooRep"
-midRep="midRep"
+
+#-----------------------------------
+#   set output format
+#-----------------------------------
+PIX_FORMAT=png
+#PIX_FORMAT=svg
+
+rm -f $irName*.$PIX_FORMAT   # remove FORMAT pictures, if they exist
+
+#-----------------------------------
+#   list of datasets
+#-----------------------------------
 nonRep="nonRep"
+midRep="midRep"
+tooRep="tooRep"
 datasets="nonRep midRep tooRep"
 
+#-----------------------------------
+#   names for inverted repeat and alpha denominator
+#-----------------------------------
+irName=ir
+aName=ad
+
+#-----------------------------------
+#   list of inverted repeats and alpha denominators
+#-----------------------------------
 #invRepeats="0 1"            # list of inverted repeats
 #alphaDens="1 10 100"        # list of alpha denominators
 invRepeats="0"
 alphaDens="20"
 
-# set output format
-PIXFORMAT=png
-#PIXFORMAT=svg
-
-rm -f $irName*.$PIXFORMAT   # remove FORMAT pictures, if they exist
-
-# set names for inverted repeat, alpha denominator, max context size
-irName=ir
-aName=ad
+#-----------------------------------
+#   max context size
+#-----------------------------------
 maxCtx=21   # real: -=1
 
-# create new files to save per mutation results
+#-----------------------------------
+#   create a couple of files to save per mutation results
+#-----------------------------------
 for ir in $invRepeats
 do
     for alphaDen in $alphaDens
@@ -116,6 +142,9 @@ do
     done
 done
 
+#-----------------------------------
+#   run the program for different datasets, ir, alphaDen and context sizes
+#-----------------------------------
 for mut in `seq -s' ' 1 $numDatasets`
 do
     for dataset in "$nonRep$mut"
@@ -128,7 +157,6 @@ do
             touch $irName$ir-$aName$alphaDen-$dataset.dat
             echo -e "# ir\talpha\tctx\tbpb" >> $irName$ir-$aName$alphaDen-$dataset.dat
 
-                # execute the code with different parameters
                 for((ctx=2; ctx<$maxCtx; ++ctx))
                 do
                 ./phoenix -m t,$ctx,$alphaDen,$ir -t ./datasets/$dataset >> $irName$ir-$aName$alphaDen-$dataset.dat
@@ -144,8 +172,8 @@ do
 #set xlabel "% mutation"
 #set ylabel "bpb"
 #set key right top                   # legend position
-#set term $PIXFORMAT                 # set terminal for output picture format
-#set output "$irName$ir.$PIXFORMAT"  # set output name
+#set term $PIX_FORMAT                 # set terminal for output picture format
+#set output "$irName$ir.$PIX_FORMAT"  # set output name
 #
 #plot for [i=5:13] 'immigration.dat' using 1:(column(i)/Sum[i]) title columnhea
 #plot for [i=0:1]
@@ -164,21 +192,27 @@ do
     done
 done
 
-# show output in a figure, using gnuplot
+#-----------------------------------
+#   plot output using "gnuplot"
+#-----------------------------------
 gnuplot <<- EOF
-set xlabel "% mutation"
-set ylabel "bpb"
-set xtics 1
-set key right top                   # legend position
-set term $PIXFORMAT                 # set terminal for output picture format
-set output "$irName$ir.$PIXFORMAT"  # set output name
+set xlabel "% mutation"                 # set label of x axis
+set ylabel "bpb"                        # set label of y axis
+set xtics 1                             # set steps for x axis
+set key right top                       # legend position
+set term $PIX_FORMAT                    # set terminal for output picture format
+set output "$irName$ir.$PIX_FORMAT"     # set output name
 
 plot "$irName$ir-${aName}20-$nonRep.dat" using 1:2  with linespoints ls 7 title "ir=$ir, alpha=1/20, $nonRep"
 
 # the following line (EOF) MUST be left as it is; i.e. no space, etc
 EOF
 
-rm -rf dat              # remove "dat" folder, if it exists
+#-----------------------------------
+#   create "dat" folder to save the results of running
+#-----------------------------------
+rm -rf dat              # remove "dat" folder, if it already exists
 mkdir -p dat            # make "dat" folder
 mv $irName*.dat dat     # move all created dat files to the "dat" folder
+
 fi  # end of running the program
