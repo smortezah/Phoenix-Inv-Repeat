0a46251a64af88f789fa342d175d114f085cf188
Morteza Hosseini <s_morteza_h@yahoo.com>
2016-12-27 23:52:52 +0000

    .

--- source/def.h
+++ source/def.h
@@ -32,4 +32,23 @@ using std::array;
 //typedef unordered_map< string, array< uint64_t, ALPH_SUM_SIZE > > htable_t;
 typedef unordered_map< string, array< uint64_t, ALPHABET_SIZE > > htable_t;
 
+
+
+
+
+class Matrix
+{
+    int *array;
+    int m_width;
+public:
+    Matrix (int w, int h) : m_width(w), array(new int[w * h]) {}
+    
+    ~Matrix () { delete[] array; }
+    
+    int at (int x, int y) const { return array[ index(x, y) ]; }
+
+protected:
+    int index (int x, int y) const { return x + m_width * y; }
+};
+
 #endif //PHOENIX_DEF_H
