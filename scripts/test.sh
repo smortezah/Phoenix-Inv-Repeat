ef36bbd2015322b4c2e4ab26ae6262a63b72564a
smortezah <s_morteza_h@yahoo.com>
2017-01-04 11:00:02 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -14,9 +14,9 @@ INSTALL_XS=0            # install "XS" from Github
 INSTALL_goose=0         # install "goose" from Github
 GEN_DATASETS=0          # generate datasets using "XS"
 GEN_MUTATIONS=0         # generate mutations using "goose"
-RUN=1                   # run the program
-PLOT_RESULTS=1          # plot results using "gnuplot"
-ARCHIVE_DATA=0
+RUN=0                   # run the program
+PLOT_RESULTS=0          # plot results using "gnuplot"
+ARCHIVE_DATA=1
 
 # mutations list:   `seq -s' ' 1 10`
 #MUT_LIST="1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50"
