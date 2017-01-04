1d0b6ab51f817c71664dd97cbd97f9d11fc98a65
smortezah <s_morteza_h@yahoo.com>
2017-01-04 14:35:04 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -35,16 +35,16 @@ chromosomes="$HUMAN_CHR_PREFIX$CHR$CURR_CHR"
 #do  chromosomes+=$HUMAN_CHR_PREFIX$CHR${i}" ";   done
 #datasets="$HUMAN_CHR$CURR_CHR"
 datasets=""
-for i in {21..22} 20
+for i in {4..22} X Y
 do  datasets+=$HUMAN_CHR${i}" ";   done
 
 #datasets="tmp"
 
 
-INV_REPEATS="0"     # list of inverted repeats      "0 1"
+INV_REPEATS="0 1"     # list of inverted repeats      "0 1"
 ALPHA_DENS="1"    # list of alpha denominators    "1 20 100"
 MIN_CTX=7         # min context size
-MAX_CTX=8          # max context size   ->  real: -=1
+MAX_CTX=12          # max context size   ->  real: -=1
 
 PIX_FORMAT=png      # output format: png, svg
 #rm -f *.$PIX_FORMAT# remove FORMAT pictures, if they exist
@@ -158,20 +158,21 @@ for ir in $INV_REPEATS; do
     for alphaDen in $ALPHA_DENS; do
         for dataset in $datasets; do
 #        rm -f $IR_NAME$ir-$a_NAME$alphaDen-${dataset}.dat
-        touch $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
-        echo -e "# mut\tmin_bpb\tmin_ctx" >> $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
+#        touch $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
+#        echo -e "# mut\tmin_bpb\tmin_ctx" >> $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
             for mut in $MUT_LIST; do
 #            rm -f $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
-            touch $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
-            echo -e "# ir\talpha\tctx\tbpb\ttime(s)" >> $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
+#            touch $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
+#            echo -e "# ir\talpha\tctx\tbpb\ttime(s)" >> $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
                 for((ctx=$MIN_CTX; ctx<$MAX_CTX; ctx+=1)); do
 #                for ctx in {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; do
+echo $dataset
                 ./phoenix -m t,$ctx,$alphaDen,$ir -t datasets/${dataset}_$mut #>> $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
                 done
-                # save "min bpb" and "min ctx" for each dataset
-                minBpbCtx=$(awk 'NR==1 || $4 < minBpb {minBpb=$4; minCtx=$3}; \
-                            END {print minBpb"\t"minCtx}' $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat)
-                echo -e "  $mut\t$minBpbCtx" >> $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
+#                # save "min bpb" and "min ctx" for each dataset
+#                minBpbCtx=$(awk 'NR==1 || $4 < minBpb {minBpb=$4; minCtx=$3}; \
+#                            END {print minBpb"\t"minCtx}' $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat)
+#                echo -e "  $mut\t$minBpbCtx" >> $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
             done
         done
     done
