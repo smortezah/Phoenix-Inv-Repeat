fbf1ee82bcccea94698708890db11a3616504bfe
smortezah <s_morteza_h@yahoo.com>
2017-01-05 15:29:07 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -17,34 +17,34 @@ INSTALL_XS=0            # install "XS" from Github
 INSTALL_goose=0         # install "goose" from Github
 GEN_DATASETS=0          # generate datasets using "XS"
 GEN_MUTATIONS=0         # generate mutations using "goose"
-RUN=0                   # run the program
-PLOT_RESULTS=1          # plot results using "gnuplot"
+RUN=1                   # run the program
+PLOT_RESULTS=0          # plot results using "gnuplot"
 ARCHIVE_DATA=0          # archive data
 
 # mutations list:   `seq -s' ' 1 10`
-MUT_LIST="1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50"
-#MUT_LIST="1 2"
+#MUT_LIST="1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50"
+MUT_LIST="1"
 
 HUMAN_CHR_PREFIX="hs_ref_GRCh38.p7_"
 CHR="chr"
 HUMAN_CHR="HS"
-CURR_CHR="22"
+CURR_CHR="21"
 chromosomes="$HUMAN_CHR_PREFIX$CHR$CURR_CHR"
 #chromosomes=""
 #for i in {1..22} X Y
 #do  chromosomes+=$HUMAN_CHR_PREFIX$CHR${i}" ";   done
-#datasets="$HUMAN_CHR$CURR_CHR"
+datasets="$HUMAN_CHR$CURR_CHR"
 #datasets="tmp"
-datasets=""
-#for i in {4..22} X Y #alts unlocalized unplaced
-for i in {1..22} X Y
-do  datasets+=$HUMAN_CHR${i}" ";    done
+#datasets=""
+##for i in {4..22} X Y #alts unlocalized unplaced
+#for i in {1..22} X Y
+#do  datasets+=$HUMAN_CHR${i}" ";    done
 
 
-INV_REPEATS="0 1"     # list of inverted repeats      "0 1"
+INV_REPEATS="0"     # list of inverted repeats      "0 1"
 ALPHA_DENS="1"    # list of alpha denominators    "1 20 100"
 MIN_CTX=2         # min context size
-MAX_CTX=11          # max context size   ->  real: -=1
+MAX_CTX=3          # max context size   ->  real: -=1
 
 PIX_FORMAT=png      # output format: png, svg
 #rm -f *.$PIX_FORMAT# remove FORMAT pictures, if they exist
@@ -157,21 +157,21 @@ if [[ $RUN == 1 ]]; then
 for ir in $INV_REPEATS; do
     for alphaDen in $ALPHA_DENS; do
         for dataset in $datasets; do
-#        rm -f $IR_NAME$ir-$a_NAME$alphaDen-${dataset}.dat
-        touch $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
-        echo -e "# mut\tmin_bpb\tmin_ctx" >> $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
+##        rm -f $IR_NAME$ir-$a_NAME$alphaDen-${dataset}.dat
+#        touch $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
+#        echo -e "# mut\tmin_bpb\tmin_ctx" >> $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
             for mut in $MUT_LIST; do
 #            rm -f $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
             touch $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
             echo -e "# ir\talpha\tctx\tbpb\ttime(s)" >> $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
                 for((ctx=$MIN_CTX; ctx<$MAX_CTX; ctx+=1)); do
 #                for ctx in {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; do
-                ./phoenix -m t,$ctx,$alphaDen,$ir -t datasets/${dataset}_$mut >> $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
+                ./phoenix -m t,$ctx,$alphaDen,$ir -t datasets/${dataset}_$mut #>> $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat
                 done
-#                # save "min bpb" and "min ctx" for each dataset
-                minBpbCtx=$(awk 'NR==1 || $4 < minBpb {minBpb=$4; minCtx=$3}; \
-                            END {print minBpb"\t"minCtx}' $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat)
-                echo -e "  $mut\t$minBpbCtx" >> $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
+##                # save "min bpb" and "min ctx" for each dataset
+#                minBpbCtx=$(awk 'NR==1 || $4 < minBpb {minBpb=$4; minCtx=$3}; \
+#                            END {print minBpb"\t"minCtx}' $IR_NAME$ir-$a_NAME$alphaDen-${dataset}_$mut.dat)
+#                echo -e "  $mut\t$minBpbCtx" >> $IR_NAME$ir-$a_NAME$alphaDen-$dataset.dat
             done
         done
     done
@@ -180,9 +180,9 @@ done
 #-----------------------------------
 #   create "dat" folder and save the results
 #-----------------------------------
-##rm -fr dat              # remove "dat" folder, if it already exists
-#mkdir -p dat            # make "dat" folder
-mv ${IR_NAME}*.dat dat  # move all created dat files to the "dat" folder
+###rm -fr dat              # remove "dat" folder, if it already exists
+##mkdir -p dat            # make "dat" folder
+#mv ${IR_NAME}*.dat dat  # move all created dat files to the "dat" folder
 
 fi  # end of running the program
 
