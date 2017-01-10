9be1c4ee59c7ee35ca946d19cf147df1666acce0
smortezah <s_morteza_h@yahoo.com>
2017-01-10 10:28:44 +0000

    .

--- source/functions.cpp
+++ source/functions.cpp
@@ -163,16 +163,23 @@ void Functions::commandLineParser (int argc, char **argv)
         {
             /// seperate and save the models in a vector of strings. each model in one string
             vector< string > strModels;
-            size_t mIndex = 0;  /// index for the first character of models string
+//            size_t mIndex = 0;  /// index for the first character of models string
             /// save all models except the last model
 //            for (size_t i = 0; i != modelsParameters.size(); ++i)
-//            if (modelsParameters[ i ] == ':')
-            for (char i : modelsParameters)
-            if (i == ':')
-            {
-                strModels.push_back(modelsParameters.substr(mIndex, i - mIndex));
-                mIndex = i + 1;
-            }
+//                if (modelsParameters[ i ] == ':')
+////            for (char i : modelsParameters)
+////                if (i == ':')
+//                {
+//                    strModels.push_back(modelsParameters.substr(mIndex, i - mIndex));
+//                    mIndex = i + 1;
+//                }
+            int mIndex=0;
+            for(string::iterator i=modelsParameters.begin();i!=modelsParameters.end();++i)
+                if(*i==':')
+                {
+                    strModels.push_back(modelsParameters.substr(mIndex, *i - mIndex));
+                    mIndex = i + 1;
+                }
 
             /// save last model in multi-model input, and the only model in single model input
             strModels.push_back(modelsParameters.substr(mIndex, modelsParameters.size() - mIndex));
