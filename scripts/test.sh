d7f7a1233d18f9a5a25cef33edb75dd9b05897ce
smortezah <s_morteza_h@yahoo.com>
2017-01-13 15:56:37 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -54,8 +54,8 @@ datasets="$HUMAN_CHR$CURR_CHR"
 #do  datasets+=$HUMAN_CHR${i}" ";    done
 
 ### Human as reference, Chimpanzee as target
-REF_DATASET="";  for i in 18; do REF_DATASET+=$HUMAN_CHR${i}" "; done # reference dataset
-TAR_DATASET="";  for i in 18; do TAR_DATASET+=$CHIMP_CHR${i}" "; done # target dataset
+REF_DATASET="";  for i in 17 18; do REF_DATASET+=$HUMAN_CHR${i}" "; done # reference dataset
+TAR_DATASET="";  for i in 17 18 21; do TAR_DATASET+=$CHIMP_CHR${i}" "; done # target dataset
 #TAR_DATASET="";  for i in 21; do TAR_DATASET+=$HUMAN_CHR${i}" "; done # reference dataset
 
 
@@ -74,10 +74,10 @@ PIX_FORMAT=png          # output format: png, svg, eps, epslatex (set output x.y
 IR_LBL=i                # label for inverted repeat
 a_LBL=a                 # label for alpha denominator
 
-INV_REPEATS="1"         # list of inverted repeats      "0 1"
+INV_REPEATS="0 1"         # list of inverted repeats      "0 1"
 ALPHA_DENS="100"        # list of alpha denominators    "1 20 100"
-MIN_CTX=13              # min context size
-MAX_CTX=14              # max context size   ->  real: -=1
+MIN_CTX=20              # min context size
+MAX_CTX=21              # max context size   ->  real: -=1
 
 
 #***********************************************************
@@ -290,7 +290,7 @@ for ir in $INV_REPEATS; do
  for alphaDen in $ALPHA_DENS; do
   for refDataset in $REF_DATASET; do
    echo -e "# ref\ttar\tir\talpha\tctx\tbpb\ttime(s)" \
-        #>> $IR_LBL$ir-$a_LBL$alphaDen-$refDataset.$INF_FILE_TYPE
+        >> $IR_LBL$ir-$a_LBL$alphaDen-$refDataset.$INF_FILE_TYPE
    for tarDataset in $TAR_DATASET; do
 ##   rm -f $IR_LBL$ir-$a_LBL$alphaDen-${dataset}.$INF_FILE_TYPE
 #   touch $IR_LBL$ir-$a_LBL$alphaDen-$dataset.$INF_FILE_TYPE
@@ -304,7 +304,7 @@ for ir in $INV_REPEATS; do
 #     for ctx in {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; do
 #     ./phoenix -m t,$ctx,$alphaDen,$ir -t $FLD_archive_datasets/${dataset}_$mut -r $FLD_archive_datasets/${dataset}_$mut \
      ./phoenix -m r,$ctx,$alphaDen,$ir -t $FLD_datasets/$tarDataset -r $FLD_datasets/$refDataset \
-#               >> $IR_LBL$ir-$a_LBL$alphaDen-$refDataset.$INF_FILE_TYPE
+               >> $IR_LBL$ir-$a_LBL$alphaDen-$refDataset.$INF_FILE_TYPE
 #               >> $IR_LBL$ir-$a_LBL$alphaDen-${dataset}_$mut.$INF_FILE_TYPE
      done
 #    done
