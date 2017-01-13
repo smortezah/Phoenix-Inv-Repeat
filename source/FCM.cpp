06e0d20767599844a5da089c9699dc840fc9b820
smortezah <s_morteza_h@yahoo.com>
2017-01-13 20:31:42 +0000

    .

--- source/FCM.cpp
+++ source/FCM.cpp
@@ -229,7 +229,7 @@ void FCM::buildTable ()
 //            << (float) 1/alphaDen << '\t'
                                   << (double) 1/alphaDen << '\t'
             << (int) contextDepth << '\t'
-            << averageEntropy << '\t'
+            << averageEntropy << "\t\t"
             << averageEntropy/LOG2_ALPHABET_SIZE
 //            << '\t'
 //            << hTable.size()
@@ -431,7 +431,7 @@ void FCM::buildHashTable ()
 //            << (float) 1/alphaDen << '\t'
                                    << (double) 1/alphaDen << '\t'
             << (int) contextDepth << '\t'
-            << averageEntropy << '\t'
+            << averageEntropy << "\t\t"
             << averageEntropy/LOG2_ALPHABET_SIZE
 //            << '\t'
 //            << hTable.size()
