671b5c4cbd4aadfd516443113960a8e3ca76836a
smortezah <s_morteza_h@yahoo.com>
2017-01-14 21:06:38 +0000

    .

--- source/FCM.cpp
+++ source/FCM.cpp
@@ -230,24 +230,24 @@ void FCM::compressTarget ()
     
     
     
-    if (mode == 't')
-    {
-        htable_t hTable = getHashTable();
-#define X (table[ tarContext * ALPH_SUM_SIZE + currSymInt ])
-#define Y (table[ tarContext * ALPH_SUM_SIZE + ALPHABET_SIZE ])
-    }
-    else if (mode == 'h')
-    {
-        htable_t hTable = getHashTable();
-#define X (hTable[ tarContext ][ currSymInt ])
-#define Y (((sumNSyms) = (0));) \
-        for (uint64_t u : hTable[ tarContext ]) \
-        sumNSyms += u;\
-        )
-    }
+//    if (mode == 't')
+//    {
+//        htable_t hTable = getHashTable();
+//#define X (table[ tarContext * ALPH_SUM_SIZE + currSymInt ])
+//#define Y (table[ tarContext * ALPH_SUM_SIZE + ALPHABET_SIZE ])
+//    }
+//    else if (mode == 'h')
+//    {
+//        htable_t hTable = getHashTable();
+//#define X (hTable[ tarContext ][ currSymInt ])
+////#define Y (((sumNSyms) = (0));) \
+////        for (uint64_t u : hTable[ tarContext ]) \
+////        sumNSyms += u;\
+//        )
+//    }
 
-//    htable_t hTable = getHashTable();
-//#define X ((mode == 'h') ? (hTable[ tarContext ][ currSymInt ]) : (table[ tarContext * ALPH_SUM_SIZE + currSymInt ]))
+    htable_t hTable = getHashTable();
+#define X ((mode == 'h') ? (hTable[ tarContext ][ currSymInt ]) : (table[ tarContext * ALPH_SUM_SIZE + currSymInt ]))
 //#define Y ((mode == 'h') ? (sumNSyms = 0;for (uint64_t u : hTable[ tarContext ])    sumNSyms += u;) : (table[ tarContext * ALPH_SUM_SIZE + ALPHABET_SIZE ]))
     
     
@@ -274,8 +274,8 @@ void FCM::compressTarget ()
                 nSym     = X;
     
                 /// sum of number of symbols
-//                sumNSyms = table[ tarContext * ALPH_SUM_SIZE + ALPHABET_SIZE ];
-                sumNSyms = Y;
+                sumNSyms = table[ tarContext * ALPH_SUM_SIZE + ALPHABET_SIZE ];
+//                sumNSyms = Y;
             
                 /// P(s|c^t)
                 probability = (double) (alphaDen * nSym + 1) / (alphaDen * sumNSyms + ALPHABET_SIZE);
@@ -318,7 +318,7 @@ void FCM::compressTarget ()
                     /// sum(n_a)
 //                    sumNSyms = 0;
 //                    for (uint64_t u : hTable[ tarContext ])     sumNSyms += u;
-                Y;
+//                Y;
 //                }
             
                 /// P(s|c^t)
