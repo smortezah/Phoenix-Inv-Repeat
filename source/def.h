be198db0a94fb1ce1d456b57ed9e0304c7bd7dc5
smortezah <s_morteza_h@yahoo.com>
2017-01-14 12:47:29 +0000

    .

--- source/def.h
+++ source/def.h
@@ -19,9 +19,9 @@ using std::array;
 /***********************************************************
     constant
 ************************************************************/
-#define ALPHABET_SIZE       5           /// alphabet {A, C, N, G, T}
-#define ALPH_SUM_SIZE       6           /// size of alphabet plus 'sum' column
-#define LOG2_ALPHABET_SIZE  2.321928095 /// log2 of ALPHABET_SIZE
+#define ALPHABET_SIZE       5               /// alphabet {A, C, N, G, T}
+#define ALPH_SUM_SIZE       6               /// ALPHABET_SIZE + 1: one more column for 'sum'
+#define LOG2_ALPHABET_SIZE  2.321928095     /// log2 of ALPHABET_SIZE
 /// TODO: max context depth for building table. 12 = possible but not cost effective
 #define TABLE_MAX_CONTEXT   11
 
