5b32d7e1676f41cfe8d78333b1fe7bfeb855e33f
smortezah <s_morteza_h@yahoo.com>
2017-01-16 19:03:04 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -54,11 +54,9 @@ datasets="$HUMAN_CHR$CURR_CHR"
 #do  datasets+=$HUMAN_CHR${i}" ";    done
 
 REF_DATASET="";  for i in 21; do REF_DATASET+=$HUMAN_CHR${i}" "; done # reference dataset
-#REF_DATASET="";  for i in 21; do REF_DATASET+=$HUMAN_CHR${i}" "; done # reference dataset
+#REF_DATASET="";  for i in 1 2A 2B {3..24}; do REF_DATASET+=CHIMP_CHR{i}" "; done # reference dataset
 TAR_DATASET="";  for i in 24; do TAR_DATASET+=$CHIMP_CHR${i}" "; done # target dataset
-#TAR_DATASET="";  for i in 24; do TAR_DATASET+=$CHIMP_CHR${i}" "; done # target dataset
-
-#TAR_DATASET="";  for i in 1 2A 2B {3..24}; do TAR_DATASET+=$CHIMP_CHR${i}" "; done # target dataset
+#TAR_DATASET="";  for i in {1..24}; do TAR_DATASET+=HUMAN_CHR{i}" "; done # target dataset
 
 FILE_TYPE="fa"          # file type
 COMP_FILE_TYPE="gz"     # compressed file type
