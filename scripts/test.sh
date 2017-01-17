12aab58cec30e1fd8c190c0e23c25736e96149cd
smortezah <s_morteza_h@yahoo.com>
2017-01-17 14:33:31 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -454,7 +454,7 @@ cd $FLD_dat
 #done
 
 #paste "mat_i0-a100-HS.dat" "mat_i1-a100-HS.dat" | for i in 1; do awk '{print ${$i}-${$i+25}}'; done
-paste "mat_i0-a100-HS.dat" "mat_i1-a100-HS.dat" | tr ',' '.' | awk '{print $1+$26}'
+paste "mat_i0-a100-HS.dat" "mat_i1-a100-HS.dat" | tr ',' '.' | for i in 1; do awk '{print $i-${i+25}}'; done
 
 #paste "z1" "z2" | tr ',' '.' | awk '{print $1+$3}'
 
