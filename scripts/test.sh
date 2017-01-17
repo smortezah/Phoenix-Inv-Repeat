2bb67aada54dcb5a5b91bea889bcb5db86276194
smortezah <s_morteza_h@yahoo.com>
2017-01-17 21:30:15 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -472,7 +472,7 @@ for alphaDen in $ALPHA_DENS; do
  for ch_PT in 1 2A 2B {3..24}; do printf "%s\t" "$CHIMP_CHR$ch_PT" >> "mat_diff_$a_LBL$alphaDen-$HUMAN_CHR.$INF_FILE_TYPE"; done
  printf "\n" >> "mat_diff_$a_LBL$alphaDen-$HUMAN_CHR.$INF_FILE_TYPE"
  ### making diff matrix (Chimpanzee)
- for ch_HS in {1..24}; do printf "\t%s" "$HUMAN_CHR$ch_HS" >> "mat_diff_$a_LBL$alphaDen-$CHIMP_CHR.$INF_FILE_TYPE"; done
+ for ch_HS in {1..24}; do printf "%s\t" "$HUMAN_CHR$ch_HS" >> "mat_diff_$a_LBL$alphaDen-$CHIMP_CHR.$INF_FILE_TYPE"; done
  printf "\n" >> "mat_diff_$a_LBL$alphaDen-$CHIMP_CHR.$INF_FILE_TYPE"
  ### ($i==$i+0) in awk checks if the column is not numeric
  ### paste "z1" "z2" | tr ',' '.' | awk '{for (i=1;i<=NF/2;i++) printf "%s\t", ($i==$i+0)?$i-$(i+NF/2):$i; print ""}' > zz
@@ -483,7 +483,7 @@ for alphaDen in $ALPHA_DENS; do
         | awk '{for (i=1;i<=NF/2;i++) printf "%s\t", $i-$(i+NF/2); print ""}' >> "mat_diff_$a_LBL$alphaDen-$chr.$INF_FILE_TYPE"
  done
 
- paste "HUMAN_PAD.dat" "mat_diff_$a_LBL$alphaDen-$HUMAN_CHR.$INF_FILE_TYPE" >> "mat_diff_$a_LBL$alphaDen-$HUMAN_CHR.$INF_FILE_TYPE"
+# paste "HUMAN_PAD.dat" "mat_diff_$a_LBL$alphaDen-$HUMAN_CHR.$INF_FILE_TYPE" >> "mat_diff_$a_LBL$alphaDen-$HUMAN_CHR.$INF_FILE_TYPE"
 done
 
 cd ..
