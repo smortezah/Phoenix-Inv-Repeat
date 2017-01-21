c9d1036aa742d1dbdebb713981b73f5a6ab30d85
smortezah <s_morteza_h@yahoo.com>
2017-01-21 18:26:04 +0000

    .

--- scripts/test.sh
+++ scripts/test.sh
@@ -464,24 +464,6 @@ set cbtics scale 0 font ",11"
 
 set palette defined (0 "red", 1 "green", 2 "white")
 
-
-do for [i=0:1] {
-### Human, i0
-set output "${IR_LBL}".i."-$HUMAN_CHR-$CHIMP_CHR.$PIX_FORMAT"
-#set title "Relative compression: HS-PT\nReference: HS, Target: PT, inverted repeats: not considered"
-
-YTICS="`awk 'BEGIN{getline}{printf "%s ",$1}' "$FLD_dat/tot-${IR_LBL}0-$HUMAN_CHR-$CHIMP_CHR.$INF_FILE_TYPE"`"
-XTICS="`head -1 "$FLD_dat/tot-${IR_LBL}0-$HUMAN_CHR-$CHIMP_CHR.$INF_FILE_TYPE"`"
-set for [i=1:words(XTICS)] xtics ( word(XTICS,i) i-1 ) font ",9" rotate by 90 offset 0,-1.4
-set for [i=1:words(YTICS)] ytics ( word(YTICS,i) i-1 ) font ",9" offset 0.5,0
-
-plot "<awk -v m=i 'NR>1' '$FLD_dat/tot-${IR_LBL}m-$HUMAN_CHR-$CHIMP_CHR.$INF_FILE_TYPE' | cut -f2-" matrix with image
-}
-
-
-
-
-
 #
 #### Human, i0
 #set output "${IR_LBL}0-$HUMAN_CHR-$CHIMP_CHR.$PIX_FORMAT"
@@ -555,8 +537,8 @@ plot "<awk -v m=i 'NR>1' '$FLD_dat/tot-${IR_LBL}m-$HUMAN_CHR-$CHIMP_CHR.$INF_FIL
 
 EOF
 
-# done
-done
+ done
+#done
 
 fi  # end of $PLOT_MATRIX
 
