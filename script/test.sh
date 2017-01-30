fe4126f50fc8e97111e2cab28151fdf0a46ddeae
smortezah <s_morteza_h@yahoo.com>
2017-01-30 20:13:30 +0000

    .

--- script/test.sh
+++ script/test.sh
@@ -122,6 +122,9 @@ MIN_CTX=20              # min context-order size
 MAX_CTX=20              # max context-order size
 
 
+
+
+
 #>>>>>  download Human choromosomes and make SEQ out of FASTA
 if [[ $GET_HUMAN==1 ]]; then . $FLD_script/get_human.sh; fi
 
@@ -131,36 +134,10 @@ if [[ $GET_CHIMPANZEE==1 ]]; then . $FLD_script/get_chimpanzee.sh; fi
 #>>>>>  download Gorilla choromosomes and make SEQ out of FASTA
 if [[ $GET_GORILLA==1 ]]; then . $FLD_script/get_gorilla.sh; fi
 
+#>>>>>  download Chicken choromosomes and make SEQ out of FASTA
+if [[ $GET_CHICKEN==1 ]]; then . $FLD_script/get_chicken.sh; fi
 
 
-
-#***********************************************************
-#   download Chicken choromosomes and make SEQ out of FASTA
-#***********************************************************
-if [[ $GET_CHICKEN == 1 ]]; then
-
-###*** download FASTA
-for i in {1..28} {30..33} LGE64 MT W Z; do
- wget $CHICKEN_URL/$CHICKEN_CHROMOSOME$i.$FILE_TYPE.$COMP_FILE_TYPE;
- gunzip < $CHICKEN_CHROMOSOME$i.$FILE_TYPE.$COMP_FILE_TYPE > $FLD_chromosomes/$CHICKEN_CHR$i.$FILE_TYPE;
- rm $CHICKEN_CHROMOSOME$i.$FILE_TYPE.$COMP_FILE_TYPE
-done
-
-for i in unlocalized unplaced; do
- wget $CHICKEN_URL/$CHICKEN_CHR_PREFIX$i.$FILE_TYPE.$COMP_FILE_TYPE;
- gunzip < $CHICKEN_CHR_PREFIX$i.$FILE_TYPE.$COMP_FILE_TYPE > $FLD_chromosomes/$CHICKEN_CHR$i.$FILE_TYPE;
- rm $CHICKEN_CHR_PREFIX$i.$FILE_TYPE.$COMP_FILE_TYPE
-done
-
-###*** rename: GGALGE64 -> GGALG, GGAunlocalized -> GGAUL, GGAunplaced -> GGAUP
-mv $FLD_chromosomes/$CHICKEN_CHR"LGE64".$FILE_TYPE $FLD_chromosomes/$CHICKEN_CHR"LG".$FILE_TYPE
-mv $FLD_chromosomes/$CHICKEN_CHR"unlocalized".$FILE_TYPE $FLD_chromosomes/$CHICKEN_CHR"UL".$FILE_TYPE
-mv $FLD_chromosomes/$CHICKEN_CHR"unplaced".$FILE_TYPE $FLD_chromosomes/$CHICKEN_CHR"UP".$FILE_TYPE
-
-###*** FASTA -> SEQ
-for i in $GGA_SEQ_RUN; do grep -v ">" $FLD_chromosomes/$CHICKEN_CHR$i.$FILE_TYPE > $FLD_datasets/$CHICKEN_CHR$i; done
-
-fi  # end of $GET_CHICKEN
 
 
 #***********************************************************
