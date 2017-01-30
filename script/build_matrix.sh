b47e6766b96cc2c2bc33509df51b68d420fb279e
smortezah <s_morteza_h@yahoo.com>
2017-01-30 19:59:13 +0000

    .

---
+++ script/build_matrix.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+
+cd $FLD_dat
+
+for i in $TAR_SEQ_RUN; do printf "\t%s" "$TAR_SPECIE$i" >> "${TAR_SPECIE}_HORIZ_PAD"; done;    echo >> "${TAR_SPECIE}_HORIZ_PAD"
+
+for alphaDen in $ALPHA_DENS; do
+ for i in $INV_REPEATS; do
+  cat "${TAR_SPECIE}_HORIZ_PAD" >> "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
+
+  for c in $REF_SEQ_RUN; do
+   awk -F "\t" '{print $7}' "$IR_LBL$i-$REF_SPECIE$c-$TAR_SPECIE.$INF_FILE_TYPE" \
+        | awk -v ref_ch=$REF_SPECIE$c 'NR == 1 {print ref_ch; next} {print}' | tr '\n' '\t' | tr ',' '.' \
+        >> "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
+   echo >> "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
+  done
+
+ ###*** filter NRC values greater than 1
+  awk 'NR>1 ' tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.dat \
+   | awk '{for (i=1;i<=NF;i++) if($i!=$i+0) printf "%s\t", $i; \
+			              else if($i==$i+0 && $i>1) printf "%.5f\t", 1; \
+ 		   	              else if($i==$i+0 && $i<=1) printf "%.5f\t", $i; \
+ 		   	              print ""}' \
+   > temp;
+  cat "${TAR_SPECIE}_HORIZ_PAD" > "temp-tot"
+  cat "temp" >> "temp-tot"
+  rm -f "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.dat";
+  mv "temp-tot" "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.dat";
+  rm -f "temp" "temp-tot"
+ done
+
+#       | tr ',' '.' | awk 'NR == 1 {print; next} {print}' \
+#       | awk '{for (i=1;i<=NF/2;i++) printf "%s\t", ($i==$i+0)?$i-$(i+NF/2):$i; print ""}' \
+ paste "tot-${IR_LBL}0-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE" "tot-${IR_LBL}1-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE" \
+       | tr ',' '.' \
+       | awk '{for (i=1;i<=NF/2;i++) if($i==$i+0) {printf "%0.5f\t", $i-$(i+NF/2);} else if($i!=$i+0) {printf "%s\t", $i;} print ""}' \
+       > "diff-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
+
+ echo -e "\t$(cat "diff-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE")" > "diff-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
+done
+
+rm -f *HORIZ_PAD
+
+cd ..
--- script/test.sh
+++ script/test.sh
@@ -512,56 +512,9 @@ done
 fi  #end of $PLOT_RESULTS
 
 
-#***********************************************************
-#   build matrix from Reference Target correspondence
-#***********************************************************
-if [[ $BUILD_MATRIX == 1 ]]; then
 
-cd $FLD_dat
-
-for i in $TAR_SEQ_RUN; do printf "\t%s" "$TAR_SPECIE$i" >> "${TAR_SPECIE}_HORIZ_PAD"; done;    echo >> "${TAR_SPECIE}_HORIZ_PAD"
-
-for alphaDen in $ALPHA_DENS; do
- for i in $INV_REPEATS; do
-  cat "${TAR_SPECIE}_HORIZ_PAD" >> "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
-
-  for c in $REF_SEQ_RUN; do
-   awk -F "\t" '{print $7}' "$IR_LBL$i-$REF_SPECIE$c-$TAR_SPECIE.$INF_FILE_TYPE" \
-        | awk -v ref_ch=$REF_SPECIE$c 'NR == 1 {print ref_ch; next} {print}' | tr '\n' '\t' | tr ',' '.' \
-        >> "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
-   echo >> "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
-  done
-
- ###*** filter NRC values greater than 1
-  awk 'NR>1 ' tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.dat \
-   | awk '{for (i=1;i<=NF;i++) if($i!=$i+0) printf "%s\t", $i; \
-			              else if($i==$i+0 && $i>1) printf "%.5f\t", 1; \
- 		   	              else if($i==$i+0 && $i<=1) printf "%.5f\t", $i; \
- 		   	              print ""}' \
-   > temp;
-  cat "${TAR_SPECIE}_HORIZ_PAD" > "temp-tot"
-  cat "temp" >> "temp-tot"
-  rm -f "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.dat";
-  mv "temp-tot" "tot-$IR_LBL$i-$REF_SPECIE-$TAR_SPECIE.dat";
-  rm -f "temp" "temp-tot"
- done
-
-#       | tr ',' '.' | awk 'NR == 1 {print; next} {print}' \
-#       | awk '{for (i=1;i<=NF/2;i++) printf "%s\t", ($i==$i+0)?$i-$(i+NF/2):$i; print ""}' \
- paste "tot-${IR_LBL}0-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE" "tot-${IR_LBL}1-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE" \
-       | tr ',' '.' \
-       | awk '{for (i=1;i<=NF/2;i++) if($i==$i+0) {printf "%0.5f\t", $i-$(i+NF/2);} else if($i!=$i+0) {printf "%s\t", $i;} print ""}' \
-       > "diff-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
-
- echo -e "\t$(cat "diff-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE")" > "diff-$REF_SPECIE-$TAR_SPECIE.$INF_FILE_TYPE"
-done
-
-rm -f *HORIZ_PAD
-
-cd ..
-
-fi  # end of $BUILD_MATRIX
-
+#>>>>>  build matrix from Reference Target correspondence
+if [[ $BUILD_MATRIX==1 ]]; then . $FLD_script/build_matrix.sh; fi
 
 #>>>>>  plot matrix from Reference Target correspondence
 if [[ $PLOT_MATRIX==1 ]]; then . $FLD_script/plot_matrix.sh; fi
