6c50543a66f2035eafcabb85c8954f3c5c3a7055
smortezah <s_morteza_h@yahoo.com>
2017-01-30 19:54:01 +0000

    .

--- script/test.sh
+++ script/test.sh
@@ -567,6 +567,10 @@ fi  # end of $BUILD_MATRIX
 #***********************************************************
 #   plot matrix from Reference Target correspondence
 #***********************************************************
+
+
+
+#>>>    plot matrix from Reference Target correspondence
 if [[ $PLOT_MATRIX==1 ]]; then . $FLD_script/plot_matrix.sh; fi
 
 
