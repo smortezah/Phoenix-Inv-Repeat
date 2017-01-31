25cbeeea13e31d17ca300819f79db7fe590c6f40
smortezah <s_morteza_h@yahoo.com>
2017-01-31 20:40:13 +0000

    .

--- script/plot_matrix.sh
+++ script/plot_matrix.sh
@@ -33,7 +33,7 @@ set cbtics scale 0.5 @fontTics offset -0.6,0
 #set cbrange [ 0.2 : 1 ] noreverse nowriteback
 
 #set palette defined (0 "red", 1 "yellow", 2 "green", 3 "white")
-set palette defined (0 "red", 1 "green", 2 "white")
+set palette defined (0 "red", 0.5 "green", 1 "white")
 
 #AxisNumScale=0.35   # axis numbers scale
 #
