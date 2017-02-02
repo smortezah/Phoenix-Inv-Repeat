68c009f362da4f76a36b51144889f1a2d9213eba
smortezah <s_morteza_h@yahoo.com>
2017-02-02 14:24:33 +0000

    .

--- script/plot_matrix_archaea.sh
+++ script/plot_matrix_archaea.sh
@@ -6,13 +6,13 @@
  for alphaDen in $ALPHA_DENS; do
 
 gnuplot <<- EOF
-set terminal $PIX_FORMAT enhanced color size 8.3,3.7
+set terminal $PIX_FORMAT enhanced color size 8.3,4.3
 set output "$REF_SPECIES-$TAR_SPECIES.$PIX_FORMAT"
-set multiplot layout 1,2 columnsfirst margins 0.02,0.925,0.23,0.992 spacing 0.03,0 # GGA-MGA, MGA-GGA
+set multiplot layout 1,2 columnsfirst margins 0.02,0.925,0.23,0.992 spacing 0.03,0
 
 #set offset 0,0,graph 0.1, graph 0.1
 xticsOffset=0.2
-yticsOffset=-0.875 #-2.3
+yticsOffset=-4
 xlabelOffset=2.75
 ylabelOffset=0 #-2.3
 #set size ratio .9 #0.85
@@ -45,7 +45,7 @@ set palette defined (0 "red", 0.5 "green", 1 "white")
 #set title "Inverted repeats considered"
 #set title "Relative compression: HS-PT\nReference: HS, Target: PT, inverted repeats: not considered"
 unset colorbox      # remove color palette
-set rmargin 65
+set rmargin 67
 
 set xtics( "Th. sp." 0, "Th. bar. s." 1, "S. is. M.16." 2, "M. brk. CM1" 3, "M. maz. C16" 4, \
            "S. is. HVE." 5, "M. sp. WWM." 6, "M. brk. MS" 7, "M. brk. s. W." 8, "M. sp. WH1" 9, "M. brk. 227" 10, \
@@ -70,7 +70,7 @@ plot "<awk 'NR>1' '$FLD_dat/tot-${IR_LBL}0-$REF_SPECIES-$TAR_SPECIES.$INF_FILE_T
 #set title "Inverted repeats not considered"
 #set title "Relative compression: HS-PT\nReference: HS, Target: PT, inverted repeats: considered"
 set colorbox        # draw color palette
-set lmargin 57
+set lmargin 62
 
 set ytics( "Th. sp." 0, "Th. bar. s." 1, "S. is. M.16." 2, "M. brk. CM1" 3, "M. maz. C16" 4, \
            "S. is. HVE." 5, "M. sp. WWM." 6, "M. brk. MS" 7, "M. brk. s. W." 8, "M. sp. WH1" 9, "M. brk. 227" 10, \
@@ -79,7 +79,7 @@ set ytics( "Th. sp." 0, "Th. bar. s." 1, "S. is. M.16." 2, "M. brk. CM1" 3, "M. 
            "M. brk. s. F." 21, "S. is. REY." 22, "Th. bar. MP" 23, "S. is. L.D." 24, "S. is. Y.N." 25, \
            "S. is. M.14." 26, "M. marip. C6" 27, "M. marip. C7" 28, "M. marip. C5" 29, "M. maz. s. G." 30, \
            "P. fu. DSM" 31, "S. solf." 32, "H. sp." 33 \
-         ) @fontTics offset yticsOffset,0
+         ) center @fontTics offset yticsOffset,0
 
 unset ylabel
 
