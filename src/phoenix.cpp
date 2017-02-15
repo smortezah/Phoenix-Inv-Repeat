f2a4f46fddd39d472264f5e873c8f10898564b72
smortezah <s_morteza_h@yahoo.com>
2017-02-15 11:38:39 +0000

    .

--- src/phoenix.cpp
+++ src/phoenix.cpp
@@ -31,45 +31,45 @@ void bar(int i){cout<<"bar "<<i<<'\n';}
 int32_t main (int argc, char *argv[])
 {
     
-    std::thread first(foo);
-    std::thread second(bar,2);
-    
-//    std::cout << "main, foo and bar now execute concurrently...\n";
-    
-    // synchronize threads:
-    first.join();                // pauses until first finishes
-    second.join();               // pauses until second finishes
-    
-//    std::cout << "foo and bar completed.\n";
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//    /// Record start time
-//    high_resolution_clock::time_point exeStartTime = high_resolution_clock::now();
+//    std::thread first(foo);
+//    std::thread second(bar,2);
 //
-//    /// for access to Functions (object 'function' on memory stack)
-//    Functions function;
-//    function.commandLineParser(argc, argv); /// parse the command line
+////    std::cout << "main, foo and bar now execute concurrently...\n";
+//
+//    // synchronize threads:
+//    first.join();                // pauses until first finishes
+//    second.join();               // pauses until second finishes
+//
+////    std::cout << "foo and bar completed.\n";
 //
 //
 //
-//
-//
-//
-//    /// Record end time
-//    high_resolution_clock::time_point exeFinishTime = high_resolution_clock::now();
-//
-//    /// calculate and show duration in seconds
-//    std::chrono::duration< double > elapsed = exeFinishTime - exeStartTime;
-//
-//    cout << '\t' << std::fixed << setprecision(2) << elapsed.count() << '\n';
+    
+    
+    
+    
+    
+    
+    
+    /// Record start time
+    high_resolution_clock::time_point exeStartTime = high_resolution_clock::now();
+
+    /// for access to Functions (object 'function' on memory stack)
+    Functions function;
+    function.commandLineParser(argc, argv); /// parse the command line
+
+
+
+
+
+
+    /// Record end time
+    high_resolution_clock::time_point exeFinishTime = high_resolution_clock::now();
+
+    /// calculate and show duration in seconds
+    std::chrono::duration< double > elapsed = exeFinishTime - exeStartTime;
+
+    cout << '\t' << std::fixed << setprecision(2) << elapsed.count() << '\n';
     
     
     return 0;
