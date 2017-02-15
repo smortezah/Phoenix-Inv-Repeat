d0514b7268ada65996612782061e614a1c30262b
smortezah <s_morteza_h@yahoo.com>
2017-02-15 18:55:09 +0000

    .

--- src/functions.cpp
+++ src/functions.cpp
@@ -262,12 +262,13 @@ void Functions::commandLineParser (int argc, char **argv)
             
             /// save all model parameters except the last model
             for (uint8_t i = parIndex; i--;)
+            {
                 if (modelParameters[ i ] == ',')
                 {
-                    vecParameters.push_back(modelParameters.substr(i+1, parIndex-i-1));
+                    vecParameters.push_back( modelParameters.substr(i+1, parIndex-i-1) );
                     parIndex = i;
                 }
-            
+            }
             /// save last model parameter
             vecParameters.push_back(modelParameters.substr(0, parIndex));
             
