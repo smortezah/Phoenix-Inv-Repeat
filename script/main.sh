52a87f71b9f914c1e660628ce2b24c248c20bd9e
smortezah <s_morteza_h@yahoo.com>
2017-02-15 15:58:02 +0000

    .

--- script/main.sh
+++ script/main.sh
@@ -69,9 +69,8 @@ TAR_SPECIES_LEN=${#TAR_SPECIES};            # length of string TAR_SPECIES
 
 
 INV_REPEATS="0"       # list of inverted repeats
-#ALPHA_DENS="100"        # list of alpha denominators
 ALPHA_DENS="100"        # list of alpha denominators
-CTX=10
+CTX=5
 MIN_CTX=10              # min context-order size
 MAX_CTX=11              # max context-order size
 
