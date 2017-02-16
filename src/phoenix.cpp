41d5669c3bb7498b066938f828d4cf16f9df6147
smortezah <s_morteza_h@yahoo.com>
2017-02-16 16:53:08 +0000

    .

--- src/phoenix.cpp
+++ src/phoenix.cpp
@@ -28,21 +28,21 @@ using std::setprecision;
 
 #include <mutex>
 std::mutex mu;
-class stmm
-{
-public:
+//class stmm
+//{
+//public:
     void foo(){ mu.lock();  cout<<"a b"<<'\n';  mu.unlock(); }
     void bar(int i){ mu.lock(); cout<<"x y"<<'\n';  mu.unlock(); }
     void mori(){ mu.lock();  cout<<"1 2"<<'\n';  mu.unlock(); }
-};
+//};
 
 int32_t main (int argc, char *argv[])
 {
-    stmm st;
+//    stmm st;
     
-    std::thread first(stmm::foo);
-    std::thread second(st.bar,3);
-    std::thread third(st.mori);
+    std::thread first(foo);
+    std::thread second(bar,3);
+    std::thread third(mori);
 
 //    std::cout << "main, foo and bar now execute concurrently...\n";
     
