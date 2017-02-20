3a0dd0c02005c12ebfeb1a231166108ed92ff2d3
smortezah <s_morteza_h@yahoo.com>
2017-02-20 09:13:05 +0000

    .

--- script/main.sh
+++ script/main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd ..
-cmake src
+cmake .
 make
 
 ### folders to be used
@@ -48,7 +48,7 @@ tempRefSeqRun=${REF_SPECIES}_SEQ_RUN;    REF_SEQ_RUN=${!tempRefSeqRun}
 REF_DATASET="";  for i in 21; do REF_DATASET+=$REF_SPECIES${i}" "; done
 #REF_DATASET="";  for i in $REF_SEQ_RUN; do REF_DATASET+=$REF_SPECIES${i}" "; done
 
-multiRef="";  for i in MT 21; do multiRef+=$FLD_dataset/$REF_SPECIES${i}" "; done
+multiRef="";  for i in MT; do multiRef+=$FLD_dataset/$REF_SPECIES${i}" "; done
 MULTIREF_DATASET="$(echo $multiRef | sed 's/ /,/g')"
 #MULTIREF_DATASET="$FLD_dataset/HS21,$FLD_dataset/PT21,"
 
@@ -69,17 +69,17 @@ tempTarSeqRun=${TAR_SPECIES}_SEQ_RUN;    TAR_SEQ_RUN=${!tempTarSeqRun}
 TAR_DATASET="";  for i in 21; do TAR_DATASET+=$TAR_SPECIES${i}" "; done
 #TAR_DATASET="";  for i in $TAR_SEQ_RUN; do TAR_DATASET+=$TAR_SPECIES${i}" "; done
 
-multiTar="";  for i in 21; do multiTar+=$FLD_dataset/$TAR_SPECIES${i}" "; done
+multiTar="";  for i in MT,MT,MT,MT; do multiTar+=$FLD_dataset/$TAR_SPECIES${i}" "; done
 MULTITAR_DATASET="$(echo $multiTar | sed 's/ /,/g')"
 #MULTITAR_DATASET="$FLD_dataset/HS21,$FLD_dataset/PT21,"
 
 TAR_SPECIES_LEN=${#TAR_SPECIES};            # length of string TAR_SPECIES
 ((TAR_SPECIES_LEN_IND=TAR_SPECIES_LEN+1));  # index of length of string TAR_SPECIES
 
-N_THREADS=7             # number of threads
+N_THREADS=4             # number of threads
 INV_REPEATS="0"       # list of inverted repeats
 ALPHA_DENS="100"        # list of alpha denominators
-CTX=10
+CTX=10                  # context-order size
 MIN_CTX=10              # min context-order size
 MAX_CTX=11              # max context-order size
 
