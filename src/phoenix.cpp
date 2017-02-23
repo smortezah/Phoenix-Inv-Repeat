9b6f8f61ad82562d3f04d4bc31d3e5216c147de8
smortezah <s_morteza_h@yahoo.com>
2017-02-23 20:30:43 +0000

    .

--- src/phoenix.cpp
+++ src/phoenix.cpp
@@ -32,11 +32,17 @@ int main (int argc, char *argv[])
     high_resolution_clock::time_point exeStartTime = high_resolution_clock::now();  /// Record start time
 
     Functions function;                     /// object 'function' on memory stack
-    function.commandLineParser(argc, argv); /// parse the command line
+//    function.commandLineParser(argc, argv); /// parse the command line
     
-//    std::vector<int> v;v.push_back(3);v.push_back(7);
-//    for(std::vector<int>::iterator i=v.begin();i!=v.end();++i)
-//        cout <<*i;
+    
+//    std::vector<string> v;v.push_back("hi");v.push_back("bye");
+//    for(std::vector<string>::iterator i=v.begin();i!=v.end();++i)
+//        if (*i == 'i')
+//            {
+//                cout <<*i<<' ';
+////                model.pushBackTarAddresses(tarFilesNames.substr(i + 1, tarIndex - it - 1));
+////                tarIndex = it;
+//            }
     
     
     
